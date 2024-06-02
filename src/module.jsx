b84163dfb8dece4754c53554d07fcd679a621e8a
Dzmitry Yarasheuski <materik08082020@gmail.com>
2024-06-03 02:10:53 +0300

    config webpack build

---
+++ src/module.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import pizza from "./img/pizza-with-shrimps.png";
+
+const Module = () => (
+  <>
+    <h1>Hello React</h1>
+    <img src={pizza} />
+  </>
+);
+
+export default Module;
